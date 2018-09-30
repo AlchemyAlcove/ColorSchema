91fb92ee314f4e4065e17afe4027809976acdd25
Mike Hoffert <mike.hoffert@gmail.com>
2018-09-30 15:22:34 -0600

    isLight and isDark function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
-export default function() {
-  return("test");
+import HexToHsl from "@alchemyalcove/rgb-to-hsl";
+
+export default class{
+  constructor(hex) {
+    this.color = this.hexToRgb(hex);
+  }
+
+  hexToRgb(hex) {
+    if(typeof hex !== "string") {
+      throw new TypeError("Expected a string");
+    }
+
+    hex = hex.replace(/^#/, "");
+
+    if(hex.length === 3) {
+      hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+    }
+
+    let num = parseInt(hex, 16);
+
+    return([num >> 16, num >> 8 & 255, num & 255]);
+  }
+
+  isDark() {
+    const hsl = HexToHsl(this.color);
+    return(hsl[2] < 50);
+  }
+
+  isLight() {
+    const hsl = HexToHsl(this.color);
+    console.log(hsl[2]);
+    return(hsl[2] >= 50);
+  }
+
+  toHex() {
+    const red = this.color[0];
+    const green = this.color[1];
+    const blue = this.color[2];
+
+    return("#" + ((blue | green << 8 | red << 16) | 1 << 24).toString(16).slice(1).toUpperCase());
+  }
 }
--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,22 @@
-import Convert from "../src";
+import Color from "../src";
+console.log(Color);
 
-test("test", () => {
-  expect(Convert()).toEqual("test");
+test("in and out", () => {
+  expect(new Color("#2CF366").toHex()).toEqual("#2CF366");
+});
+
+test("is light when not light", () => {
+  expect(new Color("#666666").isLight()).toEqual(false);
+});
+
+test("is light when not light", () => {
+  expect(new Color("#999999").isLight()).toEqual(true);
+});
+
+test("is dark when dark", () => {
+  expect(new Color("#666666").isDark()).toEqual(true);
+});
+
+test("is dark when not dark", () => {
+  expect(new Color("#999999").isDark()).toEqual(false);
 });
