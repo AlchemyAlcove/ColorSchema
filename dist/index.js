222d091dfb66d67cb44159fe254ada8584e4bbb9
Mike Hoffert <mike.hoffert@gmail.com>
2018-10-01 07:04:34 -0600

    Dist files

---
+++ dist/index.js
@@ -0,0 +1,92 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+exports.default = function (schema) {
+  if ((0, _isNil2.default)(schema) || Object.prototype.toString(schema) !== "[object Object]") {
+    schema = {};
+  }
+
+  schema = calcColor("background", schema);
+  schema = calcColor("text", schema);
+  schema = calcColor("danger", schema);
+  schema = calcColor("dangerHighlight", schema);
+  schema = calcColor("textOnDanger", schema);
+  schema = calcColor("textOnDangerHighlight", schema);
+  schema = calcColor("grayedOut", schema);
+  schema = calcColor("primary", schema);
+  schema = calcColor("primaryHighlight", schema);
+  schema = calcColor("textOnPrimary", schema);
+  schema = calcColor("textOnPrimaryHighlight", schema);
+  schema = calcColor("secondary", schema);
+  schema = calcColor("secondary", schema);
+  schema = calcColor("secondaryHighlight", schema);
+  schema = calcColor("textOnSecondary", schema);
+  schema = calcColor("textOnSecondaryHighlight", schema);
+
+  return schema;
+};
+
+var _colorCalculator = require("@alchemyalcove/color-calculator");
+
+var _colorCalculator2 = _interopRequireDefault(_colorCalculator);
+
+var _isNil = require("./isNil");
+
+var _isNil2 = _interopRequireDefault(_isNil);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var changeColor = function changeColor(key, schema) {
+  switch (key) {
+    case "background":
+      return "#ECF0F1";
+    case "danger":
+      return "#C0392B";
+    case "dangerHighlight":
+      return highlight(schema.danger);
+    case "grayedOut":
+      return "#7F8C8D";
+    case "primary":
+      return "#2C3E50";
+    case "primaryHighlight":
+      return highlight(schema.primary);
+    case "secondary":
+      return "#7F8C8D";
+    case "secondaryHighlight":
+      return highlight(schema.secondary);
+    case "text":
+      return textColor(schema.background);
+    case "textOnDanger":
+      return textColor(schema.danger);
+    case "textOnDangerHighlight":
+      return textColor(schema.dangerHighlight);
+    case "textOnPrimary":
+      return textColor(schema.primary);
+    case "textOnPrimaryHighlight":
+      return textColor(schema.primaryHighlight);
+    case "textOnSecondary":
+      return textColor(schema.secondary);
+    case "textOnSecondaryHighlight":
+      return textColor(schema.secondaryHighlight);
+    default:
+      return "#000000";
+  }
+};
+
+var calcColor = function calcColor(key, schema) {
+  if ((0, _isNil2.default)(schema[key])) {
+    schema[key] = changeColor(key, schema);
+  }
+  return schema;
+};
+
+var highlight = function highlight(color) {
+  return new _colorCalculator2.default(color).lighten(5).toHex();
+};
+
+var textColor = function textColor(color) {
+  return new _colorCalculator2.default(color).isLight() ? "#333333" : "#FFFFFF";
+};
