b1fcc08857933922617cf9cd03f07e13af943dd0
Mike Hoffert <mike.hoffert@gmail.com>
2018-10-01 07:18:10 -0600

    Switch red

--- test/index.test.js
+++ test/index.test.js
@@ -3,15 +3,15 @@ import Schema from "../src";
 test("Schema from one value", () => {
   expect(Schema({primary: "#2CF366"})).toEqual({
     "background": "#ECF0F1",
-    "danger": "#C0392B",
-    "dangerHighlight": "#D14233",
+    "danger": "#E74C3C",
+    "dangerHighlight": "#E96052",
     "grayedOut": "#7F8C8D",
     "primary": "#2CF366",
     "primaryHighlight": "#44F477",
     "secondary": "#7F8C8D",
     "secondaryHighlight": "#8C9899",
     "text": "#333333",
-    "textOnDanger": "#FFFFFF",
+    "textOnDanger": "#333333",
     "textOnDangerHighlight": "#333333",
     "textOnPrimary": "#333333",
     "textOnPrimaryHighlight": "#333333",
